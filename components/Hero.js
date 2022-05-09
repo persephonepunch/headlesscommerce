4c670958f8024d855222e543d0fce5094f22224d
yoonsun lee <yoonsunlee150@gmail.com>
2022-05-09 09:48:00 -0400

    first commit

---
+++ components/Hero.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function Hero() {
+  return (
+    <div className="my-48 mx-auto max-w-7xl px-4 sm:mt-24 md:mt-72 text-center">
+      
+    </div>
+  )
+}
